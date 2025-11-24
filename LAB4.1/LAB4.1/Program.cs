5df077e1d5d239ac2ded3d076825be4067154401
Vitos <rustamminis@gmail.com>
2025-11-24 11:30:22 +0200

    Add files via upload

---
+++ LAB4.1/LAB4.1/Program.cs
@@ -0,0 +1,23 @@
+﻿try
+{
+    Random r = new Random();
+    int[] mas1 = new int[8];
+    for (int i = 0; i < mas1.Length; i++)
+    {
+        mas1[i] = r.Next(10,99);
+        Console.Write($"{mas1[i]} ");
+    }
+    Console.WriteLine();
+    int[] mas2 = new int[8];
+    mas1.CopyTo(mas2, 0);
+    for (int i = 0; i < mas1.Length; i++)
+    {
+        mas2[i] %= 10;
+        Console.Write($"{mas2[i]} ");
+    }
+    Console.WriteLine();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
